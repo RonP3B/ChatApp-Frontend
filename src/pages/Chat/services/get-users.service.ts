654896fc20d3ca76a54c2af1918c57c9583b29cc
Roniell Pérez <71516416+RonP3B@users.noreply.github.com>
2026-07-06 10:19:00 -0400

    Add AbortSignal support to user search and cancel requests
    * refactor: add AbortSignal support to getUsers
    
    * fix: cancel pending user search requests in useMembersForm
    
    * fix: cancel pending user search requests in useNewChat

--- src/pages/Chat/services/get-users.service.ts
+++ src/pages/Chat/services/get-users.service.ts
@@ -1,6 +1,6 @@
 import { chatAppAPI } from "@/shared/APIs";
 
-export const getUsers = (username: string) => {
+export const getUsers = (username: string, signal?: AbortSignal) => {
   const ENDPOINT: string = import.meta.env.VITE_CHATAPP_USERS_ENDPOINT;
-  return chatAppAPI.get(`${ENDPOINT}?username=${username}`);
+  return chatAppAPI.get(`${ENDPOINT}?username=${username}`, { signal });
 };
